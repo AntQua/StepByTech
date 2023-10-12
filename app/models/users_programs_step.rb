3353a0e2c4d05b9ff4b6384d4d1149a7056554a0
Antonio Quaresma <tozequa@gmail.com>
2023-10-12 15:59:57 +0100

    update associations on UsersProgramsStep model

--- app/models/users_programs_step.rb
+++ app/models/users_programs_step.rb
@@ -1,5 +1,6 @@
 class UsersProgramsStep < ApplicationRecord
   belongs_to :user
   belongs_to :program
-  belongs_to :step
+  belongs_to :step # , optional: true  # Since step_id can be NULL ???
+  has_many :users_programs_steps_submissions
 end
