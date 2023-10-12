8a19cd7080afd1fb3d6f218ac645754c7d8d4a44
Antonio Quaresma <tozequa@gmail.com>
2023-10-12 15:35:31 +0100

    create Notification model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_143102) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_143203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_143102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "status"
+    t.bigint "notifications_config_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["notifications_config_id"], name: "index_notifications_on_notifications_config_id"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "notifications_configs", force: :cascade do |t|
@@ -122,6 +132,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_143102) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "notifications", "notifications_configs"
+  add_foreign_key "notifications", "users"
   add_foreign_key "program_attributes", "programs"
   add_foreign_key "steps", "programs"
   add_foreign_key "user_attributes", "program_attributes"
