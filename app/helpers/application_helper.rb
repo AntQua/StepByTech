7fc32c783d6f3dfa078fc7138f4293af42e318b7
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-10-14 19:30:41 +0100

    customize the profile floating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def sidebar_link(icon_class, text)
+        content_tag(:div, class: 'd-flex gap-2 align-items-center my-4') do
+          concat(content_tag(:i, '', class: "fa-solid #{icon_class}", style: 'color: #5e6278;'))
+          concat(content_tag(:p, text, class: 'm-0'))
+        end
+    end
+
+    def profile_menu_item(text:, icon_class:)
+        content_tag(:div, class: 'd-flex gap-2 align-items-center') do
+          concat(content_tag(:p, text))
+          concat(content_tag(:i, '', class: "fa-regular #{icon_class}", style: 'color: #5e6278;'))
+        end
+    end
 end
