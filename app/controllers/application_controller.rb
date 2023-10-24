d5fd20339a9b02012d501c3ea144906753e266e1
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-10-24 13:49:04 +0100

    Add pundit and configure program policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit::Authorization
+
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def after_sign_in_path_for(resource)
     dashboard_path
   end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  # def pundit_policy_scoped?
+  #   true # ou substitua por lógica que indica quando o scoping deve ser realizado
+  # end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Criação de um usuário administrador
+admin_user = User.create!(
+  email: 'admin@admin.com',
+  password: 'admin123',
+  is_admin: true,
+)
