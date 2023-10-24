5584d654409431f6724442b2456b7a0856bd8c36
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-10-24 17:22:51 +0100

    programs dynamic, landing page details solved

--- app/policies/program_policy.rb
+++ app/policies/program_policy.rb
@@ -4,26 +4,26 @@ class ProgramPolicy < ApplicationPolicy
     # end
     #
     def show?
-        true # Todos os usuários podem ver detalhes de um programa
+      true # Todos os usuários podem ver detalhes de um programa
     end
         #
     def new?
-        user.is_admin? # Apenas administradores podem criar novos programas
+      user.is_admin? # Apenas administradores podem criar novos programas
     end
     #
-    # def create?
-    #     new?
-    # end
+    def create?
+      new?
+    end
     #
-    # def edit?
-    #     user.is_admin? # Apenas administradores podem editar programas
-    # end
+    def edit?
+      user.is_admin? # Apenas administradores podem editar programas
+     end
     #
-    # def update?
-    #     edit? # Apenas administradores
-    # end
+    def update?
+      edit? # Apenas administradores
+    end
     #
-    # def destroy?
-    #     user.is_admin? # Apenas administradores podem excluir programas
-    # end
+    def destroy?
+     user.is_admin? # Apenas administradores podem excluir programas
+    end
 end
