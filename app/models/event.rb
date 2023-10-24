3bffb50e206cf824d13897887e309cd51c8261f7
Antonio Quaresma <tozequa@gmail.com>
2023-10-24 19:14:53 +0100

    Update the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :users_events
   has_many :users, through: :users_events
+
+  belongs_to :program, optional: true
+  belongs_to :step, optional: true
 end
