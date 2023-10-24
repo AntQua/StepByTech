12965516c1f227f921d34ce5bea3165af4408167
Antonio Quaresma <tozequa@gmail.com>
2023-10-24 19:17:26 +0100

    Update the event model add enums

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,10 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :users_events
   has_many :users, through: :users_events
-
   belongs_to :program, optional: true
   belongs_to :step, optional: true
+
+  enum status: { scheduled: 0, closed: 1 }
+  enum event_type: { online: 0, in_person: 1 }
 end
+
