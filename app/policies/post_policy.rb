36b4e7a4b4cb88a0ef6d16d4c206e10cd6abd610
Antonio Quaresma <tozequa@gmail.com>
2023-11-05 10:54:46 +0000

    setting up a policy for post

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,28 @@
+class PostPolicy
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.is_admin?
+  end
+
+  def update?
+    user.is_admin?
+  end
+
+  def destroy?
+    user.is_admin?
+  end
+end
