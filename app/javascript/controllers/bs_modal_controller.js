bb0fc8102d47d9473d6cce42ea83a2c051a78935
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-11-09 19:26:21 +0000

    fix links, images, details and start responsiveness

---
+++ app/javascript/controllers/bs_modal_controller.js
@@ -0,0 +1,18 @@
+import {Controller} from "@hotwired/stimulus";
+import {getMetaValue, loadStepsOptions} from "./helper";
+
+export default class extends Controller {
+    static targets = [];
+
+    showNewModal(event) {
+        event.preventDefault();
+        this.url = this.element.getAttribute("href");
+        fetch(this.url, {
+            headers: {
+                Accept: "text/vnd.turbo-stream.html"
+            }
+        })
+            .then(response => response.text())
+            .then(html => Turbo.renderStreamMessage(html));
+    }
+}
