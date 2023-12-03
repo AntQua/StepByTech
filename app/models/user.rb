0c3921d123b722a9ccdc646f3888eaacf577859d
Antonio Quaresma <tozequa@gmail.com>
2023-12-03 11:04:35 +0000

    update the model and controller for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,19 @@ class User < ApplicationRecord
   has_many :posts
   has_many :user_answers
 
+  validate :at_least_18
+
   has_one_attached :avatar  # This is for the Active Storage association for the user's avatar.
 
   enum gender: { Masculino: 0, Feminino: 1, Outro: 2 }
   enum country: { Portugal: 0 }
   enum city: { Lisboa: 0 }
+
+  # calculate age from the birth date
+  def age
+    return unless birth_date
+    ((Time.zone.now - birth_date.to_time) / 1.year.seconds).floor
+  end
 
   def answered_step?(step_id)
     user_answers.joins(step_question_option: { step_question: :step })
@@ -27,7 +35,18 @@ class User < ApplicationRecord
   def user_program_step(program)
     users_programs_steps.find_by(program: program)
   end
+
   def current_step(program)
     user_program_step(program)&.step
+  end
+
+  private
+
+  def at_least_18
+    return if birth_date.blank?
+
+    if ((Time.zone.now - birth_date.to_time) / 1.year.seconds).floor < 18
+      errors.add(:birth_date, 'Você deve ter pelo menos 18 anos de idade.')
+    end
   end
 end
