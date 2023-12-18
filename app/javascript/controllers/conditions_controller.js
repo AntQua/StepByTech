fe707e197f58ddb236d11224388622def423fa2f
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-12-18 23:35:02 +0000

    add validations in apply forms

--- app/javascript/controllers/conditions_controller.js
+++ app/javascript/controllers/conditions_controller.js
@@ -4,6 +4,17 @@ export default class extends Controller {
   static targets = ["applyButton"];
 
   connect() {
+    document.getElementById('applyForm').addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const isValid = this.validateQuestions();
+
+      if(isValid){
+        event.target.submit();
+      }
+      else
+        return;
+    });
   }
 
   conditionApply(event) {
@@ -17,9 +28,41 @@ export default class extends Controller {
     }
   }
 
-  toggleApplyButton() {
-    console.log('toggle');
-    const checkbox = this.element;
+  validateQuestions() {
+    const validations = [];
+
+    document.querySelectorAll("p[data-question-id]").forEach((question) => {
+      const questionId = question.dataset.questionId;
+      const questionType = question.dataset.questionType;
+      let isValid = false;
+
+      if(questionType == 0)
+      {
+        const answers = document.querySelectorAll(`textarea[data-question-id="${questionId}"]`);
+        isValid = [...answers].some(answer => answer.value.length > 0)
+      }
+      else {
+        const answers = document.querySelectorAll(
+          `input[data-question-id="${questionId}"]:checked`
+        );
   
+        isValid = answers.length > 0;
+      }
+      
+      if(isValid == false){
+        question.classList.add('text-danger')
+      }
+      else {
+        question.classList.remove('text-danger')
+      }
+
+      validations.push({ questionId, isValid });
+    });
+
+    const allIsValid = validations.every(validation => validation.isValid === true);
+
+    return allIsValid;
   }
+
+
 }
